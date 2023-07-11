50b74aad1101f4918c56ee3add956c3fd77c89a8
Lee Juwon <ku22sally@konkuk.ac.kr>
2023-07-12 08:09:32 +0900

    Update ms5611.cpp

--- MS5611/ms5611.cpp
+++ MS5611/ms5611.cpp
@@ -1,1 +1,17 @@
+#include "ms5611.h"
 
+MS5611::MS5611(PinName sda, PinName scl, PinName csb)
+    : i2c(sda,scl,csb)
+    {
+        reset(0x1E);
+        wait_ms(1);
+
+        get_atmos_pressure ();
+        get_alt();
+    }
+
+    void MS5611::reset(long reg)
+    {
+        long cmd[8] = {reg};
+        i2c.write(DEV_ADDR, &cmd, 8, 0);
+    }
