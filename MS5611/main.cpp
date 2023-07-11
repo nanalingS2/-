7996c8c6d00460193450ebe3653f9ec1a7f9c056
Lee Juwon <ku22sally@konkuk.ac.kr>
2023-07-12 08:09:46 +0900

    Update main.cpp

--- MS5611/main.cpp
+++ MS5611/main.cpp
@@ -1,1 +1,14 @@
+#include "mbed.h"
+#include "ms5611.h"
 
+Serial pc(USBTX, USBRX);
+MS5611 ms(p28, p27);
+
+int main()
+{
+    pc.baud(9600);
+
+    while (true) {
+    //    pc.printf("");
+    }
+}
