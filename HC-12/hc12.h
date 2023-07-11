fa196aa09b7e4a657a809d5015cc7d19e88dbcd8
Lee Juwon <ku22sally@konkuk.ac.kr>
2023-07-11 13:17:33 +0900

    Create hc12.h

---
+++ HC-12/hc12.h
@@ -0,0 +1,42 @@
+#ifndef __hc12_H__
+#define __hc12_H__
+
+#include "mbed.h"
+
+namespace HC12_CONST
+{
+    enum TxPower
+    {
+        TX_Power_inverse_1 = 1,
+        TX_Power_2 = 2,
+        TX_Power_5 = 3,
+        TX_Power_8 = 4,
+        TX_Power_11 = 5,
+        TX_Power_14 = 6,
+        TX_Power_17 = 7,
+        TX_Power_20 = 8 
+    };
+    
+    // char MAX_PACKET_SIZE=
+}
+
+class HC12 {
+private:
+    Serial hc12;
+
+    // const char 
+    
+
+
+public:
+    HC12(PinName TX, PinName RX);
+
+    void transmit_packet(char*data, int len);
+    // void set_default();
+    void set_tx_power(long x);
+    void set_baudrate(long baud);
+    void set_air_baudrate(long a_baud);
+
+};
+
+#endif
