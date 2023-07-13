18145d4e8c1f3f40de8e432578a38fa204191e51
Lee Juwon <ku22sally@konkuk.ac.kr>
2023-07-13 17:39:35 +0900

    Create ms5611.h

---
+++ MS5611/operated/ms5611.h
@@ -0,0 +1,52 @@
+#ifndef __MS5611_H__
+#define __MS5611_H__
+
+#include "mbed.h"
+
+namespace MS_Const
+{
+    enum cmd
+    {
+        RESET = 0b00011110,
+        ADC_READ = 0x00,
+        // PROM_READ[16] = {0xA0,0xA1,0xA2,0xA3,0xA4,0xA5,0xA6,0xA7,0xA8,0xA9,0xAA,0xAB,0xAC,0xAD,0xAE}; //0xA0 to 0xAE
+    };
+    enum ConvertD
+    {
+        D1_256 = 0x40,
+        D1_512 = 0x42,
+        D1_1024 = 0x44,
+        D1_2048 = 0x46,
+        D1_4096 = 0x48,
+
+        D2_256 = 0x50,
+        D2_512 = 0x52,
+        D2_1024 = 0x54,
+        D2_2048 = 0x56,
+        D2_4096 = 0x58
+    };
+}
+
+class MS5611
+{
+private:
+    I2C ms;
+    DigitalOut ps;
+
+    const char DEV_ADDR = (0x76<<1); //111011C: csb 보정값 = csb inverse?
+
+public:
+    MS5611(PinName sda, PinName scl, PinName ps);
+    
+    void reset(char com);
+    int get_atmos_pressure (char com);
+    void get_alt();
+
+    unsigned char crc4(unsigned int n_prom[]);
+
+    bool readable();
+    char getc();
+    void putc(char c);
+};
+
+#endif
