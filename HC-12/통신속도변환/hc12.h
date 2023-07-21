b35e116c78cc88288c748d9e453d2e5edaa0316d
Lee seoyoung <138661802+nanalingS2@users.noreply.github.com>
2023-07-21 12:01:34 +0900

    hc12.h

---
+++ HC-12/통신속도변환/hc12.h
@@ -0,0 +1,48 @@
+#ifndef __HC12_H__
+#define __HC12_H__
+
+#include "mbed.h"
+
+namespace HC12_CONST
+{
+    enum TxPower
+    {
+        TX_Power_inverse_1 = 1,
+        TX_Power_2 = 2,
+        TX_Power_5 = 3,
+        TX_Power_8 = 4,
+        TX_Power_11 = 5,
+        TX_Power_14 = 6,
+        TX_Power_17 = 7,
+        TX_Power_20 = 8 
+    };
+    
+    // char MAX_PACKET_SIZE=
+}
+
+class HC12 {
+private:
+    Serial hc12;
+    DigitalOut set;
+
+    // const char 
+    
+
+
+public:
+    HC12(PinName TX, PinName RX, PinName set);
+
+    void transmit_packet(char*data, int len);
+    void set_default(long d);
+    void set_tx_power(long x);
+    void set_baudrate(long baud);
+    void set_air_baudrate(long a_baud);
+    void set_frequency_channel(int div);
+
+    bool readable();
+    char getc();
+    void putc(char c);
+
+};
+
+#endif
